85cd3c907f5b1b65f02df7a1b185c33c15498963
Freddy Straub <straub.freddy@web.de>
2016-06-20 09:01:17 +0200

    Einstellungen Klasse

---
+++ LetsPlayTool/LetsPlayTool/Einstellungen.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LetsPlayTool
+{
+    [Serializable]
+
+    public class Einstellungen
+    {
+
+        public Einstellungen() { }
+
+        /// <summary>
+        /// Speicher die Einstellungen
+        /// </summary>
+        public void save() { }
+
+        /// <summary>
+        /// Lädt Einstellungen
+        /// </summary>
+        public void load() { }
+
+        #region Einstellungen
+
+        #region Allgemein
+        public Keys AufnahmeStartKey;
+        public Keys AufnahmeStopKey;
+
+        public bool ShowÜFenster;
+            public bool ShowÜFensterTimer;
+        
+
+        public bool ShowIErinerrung;
+
+        #endregion
+
+        #region Marker
+
+        public string MarkerSpeicherort;
+        public string MarkerFormat;
+
+        public bool MarkerKeyStrg;
+        public bool MarkerKeyShift;
+        public bool MarkerKeyAlt;
+        public Keys MarkerKey;
+
+
+
+        #endregion
+
+        #region Programme
+
+        public List<string> Programme;
+
+        #endregion
+
+        #region Timer
+
+        List<TimerProfil> TimerProfiles;
+
+
+        #endregion
+
+        #region Überwachung
+
+        public string ÜberwachungOrdner;
+
+        public bool ÜShowSpeicherort;
+        public bool ÜShowCPU;
+        public bool ÜShowGPU;
+
+        #endregion
+
+        #endregion
+
+    }
+}
---
+++ LetsPlayTool/LetsPlayTool/TimerProfil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LetsPlayTool
+{
+    public class TimerProfil
+    {
+
+        public string name { get; set; }
+        public List<ListViewItem> Times;
+        
+        public TimerProfil(string name)
+        {
+
+            this.name = name;
+
+        }
+
+
+
+    }
+}
